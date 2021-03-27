a77d7a95a23fb7c6c6c577a4867d176afe709f5a
Yusuf <hello@iamyuu.dev>
2021-03-27 14:27:26 +0700

    refactor: omit sizes and colors from Bag types

--- src/store/bag/types.ts
+++ src/store/bag/types.ts
@@ -1,6 +1,6 @@
 import { Shoes } from 'services/shoes'
 
-export interface Bag extends Shoes {
+export interface Bag extends Omit<Shoes, 'sizes' | 'colors'> {
   quantity: number
   selectedSize: string
   selectedColor: string
