8024caf9c7e7fdb287305bc0bfc518a36d34b12f
iamyuu <akuyusuf27@gmail.com>
2022-03-16 21:23:35 +0700

    chore: remove generate fake data, mock api, mock router

--- src/test/generate.ts
+++ src/test/generate.ts
@@ -1,16 +1,1 @@
-import faker from 'faker'
-import { Shoes } from 'services/shoes'
-
-export const buildShoes = (overrides?: Shoes): Shoes => ({
-  name: faker.commerce.productName,
-  category: faker.random.word,
-  description: faker.commerce.productDescription,
-  price: faker.commerce.price,
-  video: faker.internet.url,
-  sizes: Array(3).map(() => faker.random.number),
-  colors: Array(3).map(() => ({
-    name: faker.commerce.color,
-    color_hash: faker.commerce.color
-  })),
-  ...overrides
-})
+export {}
--- src/test/server/server-handlers.ts
+++ src/test/server/server-handlers.ts
@@ -1,12 +1,1 @@
-import { rest } from 'msw'
-import { buildShoes } from 'test/generate'
-
-const API_URL = process.env.NEXT_PUBLIC_API_URL
-
-export const handlers = [
-  rest.get(`${API_URL}/shoes.json`, (_, res, ctx) => {
-    const response = Array(8).map(() => buildShoes())
-
-    return res(ctx.json(response))
-  })
-]
+export const handlers = []
