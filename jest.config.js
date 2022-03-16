7720048df3a700083cc2c51da54705fd1d79abc3
iamyuu <akuyusuf27@gmail.com>
2022-03-16 17:57:13 +0700

    chore: bump deps, remove env file, use npm instead yarn

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,12 @@
-module.exports = {
-  rootDir: 'src',
-  roots: ['<rootDir>'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx'],
-  moduleDirectories: ['node_modules', 'src'],
-  setupFilesAfterEnv: ['<rootDir>/test/setup-tests.ts'],
-  testPathIgnorePatterns: ['<rootDir>[/\\\\](node_modules|.next)[/\\\\]'],
-  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
-  transform: {
-    '^.+\\.(ts|tsx)$': 'babel-jest'
-  },
-  watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname']
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './src'
+})
+
+const customJestConfig = {
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom'
 }
+
+module.exports = createJestConfig(customJestConfig)
