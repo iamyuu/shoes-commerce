7d9cf5ece4cefe3ed8ef2415d4e6829a400f6d3c
iamyuu <akuyusuf27@gmail.com>
2022-03-19 22:24:38 +0700

    perf: extract CSS on server

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,17 @@ import { DefaultSeo } from 'next-seo'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import { ChakraProvider } from '@chakra-ui/react'
+import { CacheProvider, EmotionCache } from '@emotion/react'
 import { store, persistor, wrapper } from 'store'
+import createEmotionCache from 'utils/emotion-cache'
 import theme from 'theme'
+
+interface CustomAppProps extends AppProps {
+  emotionCache?: EmotionCache
+}
+
+// client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache()
 
 export const AllProvider: React.FC = ({ children }) => (
   <Provider store={store}>
@@ -16,12 +25,14 @@ export const AllProvider: React.FC = ({ children }) => (
   </Provider>
 )
 
-function CustomApp({ Component, pageProps, router }: AppProps) {
+function CustomApp({ Component, pageProps, router, emotionCache = clientSideEmotionCache }: CustomAppProps) {
   return (
-    <AllProvider>
-      <DefaultSeo title="Shoes Commerce" canonical={router.asPath} />
-      <Component {...pageProps} />
-    </AllProvider>
+    <CacheProvider value={emotionCache}>
+      <AllProvider>
+        <DefaultSeo title="Shoes Commerce" canonical={router.asPath} />
+        <Component {...pageProps} />
+      </AllProvider>
+    </CacheProvider>
   )
 }
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,8 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import createEmotionServer from '@emotion/server/create-instance'
+import createEmotionCache from 'utils/emotion-cache'
 
 export default class CustomDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
-
   render() {
     return (
       <Html lang="en">
@@ -19,6 +16,8 @@ export default class CustomDocument extends Document {
             rel="preload"
             type="font/woff2"
           />
+          {/* inject the styles first to match with the prepend: true configuration */}
+          {(this.props as any).emotionStyleTags}
         </Head>
 
         <body>
@@ -27,5 +26,34 @@ export default class CustomDocument extends Document {
         </body>
       </Html>
     )
+  }
+}
+
+CustomDocument.getInitialProps = async ctx => {
+  const originalRenderPage = ctx.renderPage
+
+  // consider sharing the same emotion cache between all the SSR requests to speed up performance.
+  const cache = createEmotionCache()
+  const { extractCriticalToChunks } = createEmotionServer(cache)
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App: any) =>
+        function EnhanceApp(props) {
+          return <App emotionCache={cache} {...props} />
+        }
+    })
+
+  const initialProps = await Document.getInitialProps(ctx)
+  // prevents emotion to render invalid HTML.
+  // see https://github.com/mui/material-ui/issues/26561#issuecomment-855286153
+  const emotionStyles = extractCriticalToChunks(initialProps.html)
+  const emotionStyleTags = emotionStyles.styles.map(style => (
+    <style data-emotion={`${style.key} ${style.ids.join(' ')}`} key={style.key} dangerouslySetInnerHTML={{ __html: style.css }} />
+  ))
+
+  return {
+    ...initialProps,
+    emotionStyleTags
   }
 }
