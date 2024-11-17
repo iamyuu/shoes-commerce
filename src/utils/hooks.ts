b134610e80e2799e59ffe32b1937c9d4120252c5
Muhammad Yusuf <yusuf@iamyuu.dev>
2024-11-17 07:01:54 +0700

    fix: type timeout

--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -8,7 +8,7 @@ export type SomeFunction<TArg> = (...args: TArg[]) => void
  * @returns The debounced function, which will run only if the debounced function has not been called in the last (delay) ms
  */
 export function useDebounceFn<TArg>(func: SomeFunction<TArg>, delay = 500) {
-  const timer = React.useRef<NodeJS.Timeout>()
+  const timer = React.useRef<number>()
 
   React.useEffect(() => {
     return () => {
@@ -18,9 +18,9 @@ export function useDebounceFn<TArg>(func: SomeFunction<TArg>, delay = 500) {
   }, [])
 
   const debouncedFunction = ((...args) => {
-    const newTimer = setTimeout(() => func(...args), delay)
+    const timerId = setTimeout(() => func(...args), delay)
     clearTimeout(timer.current)
-    timer.current = newTimer
+    timer.current = timerId as unknown as number
   }) as SomeFunction<TArg>
 
   return debouncedFunction
